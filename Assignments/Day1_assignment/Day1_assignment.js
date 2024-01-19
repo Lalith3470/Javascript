4ef6ff8d2d326dfe81983267ee1901abe5e4305e
Lalith Kiran Molleti <lalithkiran539@gmail.com>
2024-01-19 11:53:09 +0530

    Assignment Day1

---
+++ Assignments/Day1_assignment/Day1_assignment.js
@@ -0,0 +1,85 @@
+// let a=10;
+// console.log(typeof a);
+
+
+// function tem(){
+//     console.log("Hello....");
+// }
+// tem();
+
+
+// let a=10;
+// function type(){
+//     console.log("Hello",a);
+// }
+// type();
+
+
+// let a=1;
+// function tmp(){
+//     a=10;
+//     console.log(a);
+//     {
+//         a=18;
+//         console.log(a);
+//     }
+// }
+// console.log(a);
+// tmp();
+
+
+// let x=myfun(4,3);
+// function myfun(a,b){
+//     return a*b;
+// }
+// console.log(x);
+
+
+// IF else conditions
+// function fun(n){
+//     if(n>=18){
+//         console.log("False");
+//     }
+//     else{
+//         console.log("True");
+//     }
+// }
+// fun(20);
+
+
+//switch
+
+// let a=1;
+// switch(a){
+//     case 0:
+//         console.log("Zero");
+//         break;
+//     case 1:
+//         console.log("One");
+//         break;
+//     case 2:
+//         console.log("Two");
+//         break;
+//     case 3:
+//         console.log("Three");
+//         break;
+// }
+
+// let text;
+// switch (new Date().getDay()) {
+//     case 6:
+//       text = "Today is Saturday";
+//       break;
+//     case 0:
+//       text = "Today is Sunday";
+//       break;
+//     default:
+//       text = "Looking forward to the Weekend";
+//   }
+
+
+
+function fun(a,b){
+    return a*b;
+}
+console.log(fun(8,9))
